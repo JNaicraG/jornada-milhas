86abe5ee73518e3d34b1d7db45d9c1ce0e66eb3c
GarciaNJ <nicolas.jg.njg@gmail.com>
2023-10-01 15:37:28 -0300

    feat: created card-busca-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import locale_pt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,13 +9,17 @@ import { BannerComponent } from './shared/banner/banner.component';
 import { CardComponent } from './shared/card/card.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { ContainerComponent } from './shared/container/container.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { ContainerComponent } from './shared/container/container.component';
-import { HomeComponent } from './pages/home/home.component';
+import { MatCardModule } from '@angular/material/card';
+
+registerLocaleData(locale_pt);
 
 @NgModule({
   declarations: [
@@ -23,16 +29,21 @@ import { HomeComponent } from './pages/home/home.component';
     FooterComponent,
     HeaderComponent,
     ContainerComponent,
-    HomeComponent
+    HomeComponent,
+    CardBuscaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [{
+    provide:LOCALE_ID,
+    useValue:'pt-br'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-busca',
+  templateUrl: './card-busca.component.html',
+  styleUrls: ['./card-busca.component.scss']
+})
+export class CardBuscaComponent {
+  @Input() src:string = '';
+  @Input() alt:string = '';
+  @Input() name:string = '';
+  @Input() price:string = '';
+
+}
