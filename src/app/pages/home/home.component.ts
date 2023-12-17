72596365c4ac0e59b63c6af0ce550bdfac9a0ca1
GarciaNJ <nicolas.jg.njg@gmail.com>
2023-12-17 19:13:05 -0300

    chore: implemented new depoimentos component on home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DepoimentoService } from 'src/app/core/services/depoimento.service';
 import { PromocaoService } from 'src/app/core/services/promocao.service';
-import { Promocao } from 'src/app/core/types/types';
+import { Depoimento, Promocao } from 'src/app/core/types/types';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,16 @@ import { Promocao } from 'src/app/core/types/types';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
-  constructor(private promocaoService:PromocaoService){}
+  constructor(
+    private promocaoService:PromocaoService,
+    private depoimentoService:DepoimentoService
+    ){}
   
   promocoes:Promocao[] = [];
+  depoimentos:Depoimento[] = [];
 
   ngOnInit(): void {
     this.promocaoService.listar().subscribe(lista => this.promocoes=lista);
+    this.depoimentoService.listar().subscribe(lista => this.depoimentos=lista);
   }
 }
