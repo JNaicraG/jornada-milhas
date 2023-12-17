7d06d6c07582f98fb1e49f955e026d1adbd54970
GarciaNJ <nicolas.jg.njg@gmail.com>
2023-12-17 18:32:21 -0300

    feat: completed autocomplete on dropdown-uf component

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,13 +1,51 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Observable, startWith, of , map} from 'rxjs';
+import { UnidadeFederativaService } from 'src/app/core/services/unidade-federativa.service';
+import { UnidadeFederativa } from 'src/app/core/types/types';
 
 @Component({
   selector: 'app-dropdown-uf',
   templateUrl: './dropdown-uf.component.html',
   styleUrls: ['./dropdown-uf.component.scss']
 })
-export class DropdownUfComponent {
+export class DropdownUfComponent implements OnInit{
 @Input() label:string = '' ;
 @Input() iconePrefixo:string = '' ;
+myControl = new FormControl('');
 
-filteredOptions = [];
+unidadesFederativas:UnidadeFederativa[]=[];
+unidadesFederativasFiltradas?:Observable<UnidadeFederativa[]>;
+//unidadesFederativasFiltradas:UnidadeFederativa[]=[];
+
+
+constructor(private unidadeFederativaService:UnidadeFederativaService){}
+  ngOnInit(): void {
+    this.unidadeFederativaService.listar().subscribe(listaUF => {
+      this.unidadesFederativas = listaUF ;
+      this.unidadesFederativasFiltradas = this._atualizarFiltro();
+    });
+    
+    this.unidadesFederativasFiltradas = this._atualizarFiltro(); 
+  }
+
+  private _atualizarFiltro():Observable<UnidadeFederativa[]>{
+    return this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value as string || '')),
+      );
+  }
+
+  private _filter(value:string = ''):UnidadeFederativa[]{
+    if(value === '')
+      return this.unidadesFederativas;
+
+    const filtredValue = value.toLowerCase();
+    return this.unidadesFederativas.filter(uf => uf.nome.toLowerCase().includes(filtredValue))
+   }
+
+   displayFn(uFederativa:UnidadeFederativa):string{
+    return uFederativa && uFederativa.nome ? uFederativa.nome : '';
+   }
 }
