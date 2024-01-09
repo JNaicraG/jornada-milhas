abecd301e0c99a6447f3ad9c718994ba098cb18c
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-01-09 19:40:31 -0300

    chore: updated seletor-passageiro ControlValueAccessor

--- src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
+++ src/app/shared/seletor-passageiro/seletor-passageiro.component.ts
@@ -1,18 +1,49 @@
 import { Component, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-seletor-passageiro',
   templateUrl: './seletor-passageiro.component.html',
   styleUrls: ['./seletor-passageiro.component.scss']
 })
-export class SeletorPassageiroComponent {
-
+                                                  //Responsável por implementar interface que recebe controles de formulário como param do component
+export class SeletorPassageiroComponent implements ControlValueAccessor{
+  
   @Input() titulo:string = '';
   @Input() subTitulo:string = '';
+  value:number = 0;
+  onChange = (val:number) => {}
+  onTouch =() => {}
 
+  
+  //Responsável por amazenar e escrever o valor do nosso input
+  writeValue(obj: any): void { //obj no caso é o nosso valor recebido
+    this.value = obj;
+  }
+
+  //Faz o vínculo com a alteração desse valor e o formulário dinâmico
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
 
   onSubmit(value:any){
-    console.log("AAAAAAAA" + value)
+    this.value = value;
+    this.onChange = value;
+    this.onTouch();
   }
 
 }
+
+
+/*
+writeValue(value: any): Este método é chamado pelo Angular Forms para atualizar o valor do componente personalizado com base no valor fornecido pelo formulário. O componente deve atualizar sua visualização e estado interno de acordo com o novo valor.
+registerOnChange(fn: any): Este método é usado para registrar uma função de retorno de chamada que será chamada pelo componente personalizado sempre que houver alterações em seu valor interno. O componente deve chamar essa função sempre que o valor for alterado para notificar o Angular Forms sobre as alterações.
+registerOnTouched(fn: any): Este método é usado para registrar uma função de retorno de chamada que será chamada pelo componente personalizado quando ele for tocado ou sofrer uma alteração no estado de foco. O componente deve chamar essa função sempre que ocorrer uma interação com ele, como um clique ou foco.
+setDisabledState(isDisabled: boolean): Este método é usado para definir o estado de desabilitado do componente personalizado com base no valor fornecido pelo formulário. O componente deve atualizar sua visualização e comportamento de acordo com o estado de desabilitado.
+*/
