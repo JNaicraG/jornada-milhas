06dbccfd605ec01f2b730a921e7060054640c567
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-01-26 18:44:54 -0300

    chore: integrated autenticacaoService to login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -9,17 +11,30 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit{
   loginForm!:FormGroup;
 
-  constructor(private formBuilder:FormBuilder){}
+  constructor(
+    private autenticacaoService:AutenticacaoService,
+    private route:Router,
+    private formBuilder:FormBuilder){}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: [null],
-      senha: [null]
+      email: [null, [Validators.required, Validators.email]],
+      senha: [null, [Validators.required]]
     });
   }
 
   login():void{
-    console.log("Logou: ", this.loginForm.value);
+    const email = this.loginForm.value.email;
+    const senha = this.loginForm.value.senha;
+    this.autenticacaoService.autenticar(email,senha).subscribe({
+      next: (value) => {
+        console.log("Logou com sucesso! ", value);
+        this.route.navigateByUrl('/');
+      },
+      error: (err) => {
+        console.log("Erro ao logar! ", err, email, senha);
+      }
+    })
   }
   
 }
