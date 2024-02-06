c40e0aa4978f4f8eb1446c77e754d4e2ca6f8ab0
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-02-05 23:20:55 -0300

    chore:updated cadastroService

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,6 +1,6 @@
 import { PessoaUsuaria } from './../types/types';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -15,5 +15,20 @@ export class CadastroService {
 
   cadastrar(pessoa:PessoaUsuaria):Observable<PessoaUsuaria>{
     return this.http.post<PessoaUsuaria>(`${this.apiUrl}/auth/cadastro`,pessoa);
+  }
+
+  buscarCadastro(token:string):Observable<PessoaUsuaria>{
+    const headers= new HttpHeaders({
+      'Authorization':`Bearer ${token}`
+    });
+    return this.http.get<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`,{headers});
+  }
+
+  atualizarCadastro(pessoa:PessoaUsuaria, token:string):Observable<PessoaUsuaria>{
+    const headers= new HttpHeaders({
+      'Authorization':`Bearer ${token}`
+    });
+
+    return this.http.patch<PessoaUsuaria>(`${this.apiUrl}/auth/perfil`,pessoa,{headers});
   }
 }
