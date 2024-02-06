fb072782105db04e3f2da71381e0915430896883
GarciaNJ <nicolas.jg.njg@gmail.com>
2024-02-05 21:49:04 -0300

    chore: implemented tokenService in autenticacaoService

--- src/app/core/services/autenticacao.service.ts
+++ src/app/core/services/autenticacao.service.ts
@@ -1,8 +1,13 @@
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { UserService } from '../service/user.service';
+
+interface AuthResponse {
+  access_token: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +16,15 @@ export class AutenticacaoService {
 
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userService:UserService) { }
 
-  autenticar(email:string, senha:string):Observable<any>{
-    return this.http.post<any>(`${this.apiUrl}/auth/login`, {email,senha})
+  autenticar(email: string, senha: string): Observable<HttpResponse<AuthResponse>> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/login`, { email, senha },
+      { observe: 'response' }).pipe(
+        tap((response) => {
+          const authToken = response.body?.access_token || '';
+          this.userService.salvarToken(authToken);
+        })
+      )
   }
 }
